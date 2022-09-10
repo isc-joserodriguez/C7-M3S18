e3d252f8ecb9ac52a2c804c873e750c31dcbb246
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-09-10 09:38:45 -0600

    Create & Read

--- main.js
+++ main.js
@@ -6,8 +6,25 @@
  * 5.- Levantar el servidor
  */
 
+const mascotas = [
+  {
+    tipo: "Perro",
+    edad: 5,
+    color: "Blanco",
+    peso: "8 kg",
+    nombre: "Pedro",
+  },
+  {
+    tipo: "Gato",
+    edad: 2,
+    color: "negro",
+    peso: "3 kg",
+    nombre: "Michi",
+  },
+];
+
 //! 1.- Importar todas nuestras bibliotecas
-const express = require('express');
+const express = require("express");
 
 //! 2.- Hacer instancia de la aplicación de express y <>
 const app = express();
@@ -16,11 +33,24 @@ const app = express();
 app.use(express.json());
 
 //! 4.- Declarar las rutas
-app.get("/", (req, res)=>{
-    res.json({mensaje: 'Hola'})
+app.get("/", (req, res) => {
+  res.json({ mensaje: "Hola" });
+});
+
+// CREATE - POST
+app.post("/mascota", (req, res) => {
+  const { tipo, edad, color, peso, nombre } = req.body;
+  mascotas.push({ tipo, edad, color, peso, nombre });
+  res.json({ mensaje: "Mascota registrada", data: mascotas });
+});
+
+// READ - GET
+app.get("/mascota", (req, res) => {
+  console.log("GET:", req.body);
+  res.json({ mascotas });
 });
 
 //! 5.- Levantar el servidor
-app.listen(3001, ()=>{
-    console.log('La aplicación corre en el puerto:'+ 3001)
+app.listen(3001, () => {
+  console.log("La aplicación corre en el puerto:" + 3001);
 });
